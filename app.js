67c097dc22756eebecd148408db22760f4609467
Komal Nadeem <koomalkhan111@gmail.com>
2024-10-27 21:11:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let myname=alert("komal nadeem");
+
+let age=alert("17");
+
+let course=alert("Certified Mobile Application Development.");
+
+
+let word= "Pizza";
+let result= word+"\n"+word.slice(0,4) + "\n" + word.slice(0,3) + "\n" + word.slice(0,2) +"\n" + word.slice(0,1);
+alert (result);
+
+let email= alert("My email address is example @example.com");
+
+let study=alert("I am trying to learn  from the Book A Smarter Way To Learn JavaSript.")
+
+
+document.write("Yahh! I can write HTML content through JavaScript.")
+
+
+
+let sym=alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
